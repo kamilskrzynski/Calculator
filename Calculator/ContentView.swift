5fd3cccf39f35d4fde36d415919c0166f71bffc3
KamilSkrzynski <kamilskrzynski95@gmail.com>
2022-05-30 14:26:28 +0200

    Basic UI

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -9,8 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var currentValue: String = "81"
+    
+    let buttons = [
+        ["AC", "+/-", "%", "÷"],
+        ["7", "8", "9", "x"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ",", "="],
+    ]
+    
     var body: some View {
-        Text("Hello World!")
+        ZStack(alignment: .bottom) {
+            
+            Color.black.ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    Text(currentValue)
+                        .foregroundColor(.white)
+                        .font(.system(size: 72))
+                }
+                .padding()
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack {
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                                .foregroundColor(.white)
+                                .font(.system(size: 30, weight: .semibold))
+                                .frame(width: button == "0" ? (getButtonWidth() * 2 + 12) : getButtonWidth(), height: getButtonWidth())
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(getButtonWidth())
+                        }
+                    }
+                }
+            }
+            .padding(.bottom)
+        }
+    }
+    
+    func getButtonWidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - (5*12)) / 4
     }
 }
 
